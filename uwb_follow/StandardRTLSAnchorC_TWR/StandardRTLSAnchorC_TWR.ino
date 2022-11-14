20848b692ee2f8a78afbc24d9771d5ffb2b728c9
kawaiineuya <55311723+kawaiineuya@users.noreply.github.com>
2022-11-14 16:40:02 +0900

    Add files via upload

---
+++ uwb_follow/StandardRTLSAnchorC_TWR/StandardRTLSAnchorC_TWR.ino
@@ -0,0 +1,157 @@
+#define ROSSERIAL_ARDUINO_TCP
+
+#include <WiFi.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+#include <DW1000Ng.hpp>
+#include <DW1000NgUtils.hpp>
+#include <DW1000NgRanging.hpp>
+#include <DW1000NgRTLS.hpp>
+
+const char* ssid = "Conference Room";
+const char* password = "1398134490";
+IPAddress server(192,168,0,78);
+const uint16_t serverPort = 11413;
+ros::NodeHandle nh;
+std_msgs::Float32 uwb_c_range_msg;
+std_msgs::Float32 uwb_c_rx_power_msg;
+ros::Publisher uwb_c_range("uwb_c_range", &uwb_c_range_msg);
+ros::Publisher uwb_c_rx_power("uwb_c_rx_power", &uwb_c_rx_power_msg);
+
+// connection pins
+const uint8_t PIN_SCK = 18;
+const uint8_t PIN_MOSI = 23;
+const uint8_t PIN_MISO = 19;
+const uint8_t PIN_SS = 2;
+const uint8_t PIN_RST = 15;
+const uint8_t PIN_IRQ = 17;
+
+// Extended Unique Identifier register. 64-bit device identifier. Register file: 0x01
+char EUI[] = "AA:BB:CC:DD:EE:FF:00:03"; 
+
+byte main_anchor_address[] = {0x01, 0x00};
+
+uint16_t blink_rate = 0;
+
+double range_self;
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_5,
+    DataRate::RATE_850KBPS,
+    PulseFrequency::FREQ_16MHZ,
+    PreambleLength::LEN_256,
+    PreambleCode::CODE_3
+};
+
+frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(115200);
+    Serial.print("connect try : ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED){
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+
+    nh.getHardware() -> setConnection(server, serverPort);
+    nh.initNode();
+
+    Serial.print("IP = ");
+    Serial.println(nh.getHardware() -> getLocalIP());
+
+    nh.advertise(uwb_c_range);
+    nh.advertise(uwb_c_rx_power);
+
+    Serial.println(F("### arduino-DW1000Ng-ranging-anchor-C ###"));
+    // initialize the driver
+
+    DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+
+    Serial.println(F("DW1000Ng initialized ..."));
+    // general configuration
+    DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
+    DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
+    
+    DW1000Ng::setEUI(EUI);
+
+    DW1000Ng::setPreambleDetectionTimeout(64);
+    DW1000Ng::setSfdDetectionTimeout(273);
+    DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
+
+    DW1000Ng::setNetworkId(RTLS_APP_ID);
+    DW1000Ng::setDeviceAddress(3);
+	
+    DW1000Ng::setAntennaDelay(16436);
+    
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000Ng::getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000Ng::getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);
+}
+
+void transmitRangeReport() {
+    byte rangingReport[] = {DATA, SHORT_SRC_AND_DEST, DW1000NgRTLS::increaseSequenceNumber(), 0,0, 0,0, 0,0, 0x60, 0,0 };
+    DW1000Ng::getNetworkId(&rangingReport[3]);
+    memcpy(&rangingReport[5], main_anchor_address, 2);
+    DW1000Ng::getDeviceAddress(&rangingReport[7]);
+    DW1000NgUtils::writeValueToBytes(&rangingReport[10], static_cast<uint16_t>((range_self*1000)), 2);
+    DW1000Ng::setTransmitData(rangingReport, sizeof(rangingReport));
+    DW1000Ng::startTransmit();
+}
+ 
+void loop() {
+     RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::ACTIVITY_FINISHED, blink_rate);
+     if(result.success) {
+        delay(4); // Tweak based on your hardware
+        range_self = result.range;
+        transmitRangeReport();
+
+        String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
+        rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
+        Serial.println(rangeString);
+        if (nh.connected()){
+            // Serial.println("connected");
+            uwb_c_range_msg.data = range_self;
+            uwb_c_rx_power_msg.data = DW1000Ng::getReceivePower();
+            uwb_c_range.publish(&uwb_c_range_msg);
+            uwb_c_rx_power.publish(&uwb_c_rx_power_msg);
+            // Serial.print("ok");
+        }else{
+            // Serial.println("not connected");
+        }
+        nh.spinOnce();
+     }
+}
+
